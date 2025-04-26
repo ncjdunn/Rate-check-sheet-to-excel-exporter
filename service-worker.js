3dbb4e8aff9d6de41eba07692dc153c44845c00c
ncjdunn <ncjdunn@gmail.com>
2025-04-26 16:06:23 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/main.js',
-  '/manifest.json'
+  'index.html',
+  'style.css',
+  'main.js',
+  'manifest.json'
 ];
 
 self.addEventListener('install', evt => {
@@ -17,7 +16,7 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+      keys.map(key => (key !== CACHE_NAME) ? caches.delete(key) : null)
     ))
   );
   self.clients.claim();
@@ -26,6 +25,8 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   if (evt.request.mode !== 'navigate') return;
   evt.respondWith(
-    fetch(evt.request).catch(() => caches.open(CACHE_NAME).then(cache => cache.match('index.html')))
+    fetch(evt.request).catch(() =>
+      caches.open(CACHE_NAME).then(cache => cache.match('index.html'))
+    )
   );
 });
